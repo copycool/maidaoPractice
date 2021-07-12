14d1b00f378071f75583b9e04705a48149dbf943
tianchen <2813360784@qq.com>
2021-07-12 19:29:26 +0800

    创建了edu.txt

---
+++ src/main/java/cn/maidaotech/java07/test/java/cn/maidaotech/java07/Java07ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.maidaotech.java07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
