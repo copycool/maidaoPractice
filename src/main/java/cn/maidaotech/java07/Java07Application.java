eb87aba56d2e3c4ebdbe2dd1868b791638e03e1e
Ghdzj521 <3219483712@qq.com>
2021-07-12 16:43:00 +0800

    提交测试LDY

--- src/main/java/cn/maidaotech/java07/Java07Application.java
+++ src/main/java/cn/maidaotech/java07/Java07Application.java
@@ -8,7 +8,7 @@ public class Java07Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java07Application.class, args);
-		System.out.println("testsetsets");
+		System.out.println("testssetsets");
 	}
 
 }
