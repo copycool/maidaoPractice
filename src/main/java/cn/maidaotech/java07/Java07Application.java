d501928fcdd01005558958b59b53bf7b6d99b8f8
胡乾乾 <925209748@qq.com>
2021-07-13 11:25:17 +0800

    update

--- src/main/java/cn/maidaotech/java07/Java07Application.java
+++ src/main/java/cn/maidaotech/java07/Java07Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java07Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello!");
 		SpringApplication.run(Java07Application.class, args);
 		System.out.println(".");
 	}
