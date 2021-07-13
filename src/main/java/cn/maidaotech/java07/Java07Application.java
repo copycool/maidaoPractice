3be1fcd39e8afbaa3c18ec2019a620e80d311f8e
lizhenwei <1828155655@qq.com>
2021-07-13 11:12:58 +0800

    Update Java07Application.java

--- src/main/java/cn/maidaotech/java07/Java07Application.java
+++ src/main/java/cn/maidaotech/java07/Java07Application.java
@@ -8,6 +8,7 @@ public class Java07Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java07Application.class, args);
+		//git@gitee.com:dawnlizhenwei/dawn.git
 	}
 
 }
