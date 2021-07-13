4e9e47e292b374e445d81df9a9111f093d275a84
guoteng <gtbetter666@163.com>
2021-07-13 19:16:54 +0800

    数据类型

---
+++ src/main/java/cn/maidaotech/java07/chapter01/NumberTest.java
@@ -0,0 +1,13 @@
+package cn.maidaotech.java07.chapter01;
+
+public class NumberTest {
+    public static void main(String[] args) {
+        System.out.println("int所占字节数:"+Integer. SIZE/8);
+        System.out.println("int最大值:"+Integer.MAX_VALUE);
+        System.out.println("int最小值:"+Integer.MIN_VALUE);
+        System.out.println("byte所占字节数：" + Byte.SIZE/8);
+        System.out.println("byte最小值：" + Byte.MIN_VALUE);
+        System.out.println("byte最大值：" + Byte.MAX_VALUE);
+        //short和long跟byte相似
+    }
+}
