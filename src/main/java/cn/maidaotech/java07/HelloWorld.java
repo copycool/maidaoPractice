a98cdbea3dc3254e288bdd8fde0684190c3bacba
wys <1801537964@qq.com>
2021-07-13 16:46:05 +0800

    复制

---
+++ src/main/java/cn/maidaotech/java07/HelloWorld.java
@@ -0,0 +1,7 @@
+package cn.maidaotech.java07;
+
+public class HelloWorld {
+    public static void  main(String[] args) {
+        System.out.println("你好"+Math.E);
+    }
+}
--- src/test/java/cn/maidaotech/java07/Java07ApplicationTests.java
+++ src/test/java/cn/maidaotech/java07/Java07ApplicationTests.java
@@ -8,6 +8,7 @@ class Java07ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("我是测试");
 	}
 
 }
