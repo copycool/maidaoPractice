4b16810b80c345cd81a777ed5ac64d2766c6b1d3
李鼎凯 <1803727633@qq.com>
2021-07-13 09:46:16 +0800

    第一次

--- src/main/java/cn/maidaotech/java07/Java07Application.java
+++ src/main/java/cn/maidaotech/java07/Java07Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java07Application {
 
 	public static void main(String[] args) {
+		System.out.println("第一次测试");
 		SpringApplication.run(Java07Application.class, args);
-	}
+	}	
 
 }
