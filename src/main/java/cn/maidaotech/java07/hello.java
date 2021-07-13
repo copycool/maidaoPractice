2cc23b45733eace1885e355294ccf4d8f81460f7
余代起 <2944183739@qq.com>
2021-07-13 15:01:14 +0800

    Hello.java

--- src/main/java/cn/maidaotech/java07/hello.java
+++ src/main/java/cn/maidaotech/java07/hello.java
@@ -1,9 +1,9 @@
+package cn.maidaotech.java07;
+public class hello{
 
-public class hello {
-	public static void main(String[] args) {
-        System.out.println("hello!");
+    public static void main(String[] args) {
+
+        System.out.println("hi,girl");
 		
-	
-	}
-
+    }
 }
