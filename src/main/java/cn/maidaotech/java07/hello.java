f86e32ecb7766011a795cae2d8b14ccd7dc2bfda
余代起 <2944183739@qq.com>
2021-07-13 11:30:38 +0800

    输出修改

---
+++ src/main/java/cn/maidaotech/java07/hello.java
@@ -0,0 +1,16 @@
+
+    package cn.maidaotech.java07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hello {
+	public static void main(String[] args) {
+        System.out.println("hello");
+		
+		SpringApplication.run(Java07Application.class, args);
+	}
+
+}
+}
