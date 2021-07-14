4d146c78466296e3f0d3f90726a715bcbcbc0aa7
luxiang <1264257976@qq.com>
2021-07-14 21:36:44 +0800

    zuoye

--- demo/src/demo/src/main/java/com/example/demo/UserName.java
+++ demo/src/demo/src/main/java/com/example/demo/UserName.java
@@ -2,22 +2,44 @@ package com.example.demo;
 
 import javax.swing.Spring;
 
+import org.apache.tomcat.util.buf.Ascii;
+
 public class UserName {
     public static void main(String[] args) {
      
-        System.out.println(check("name"));
+        System.out.println(check("zhangsan1234"));
+        System.out.println(check("zhangsa*n1234"));
     }
-    public static boolean check(Spring userName){
+    public static boolean check(String userName){
         if (userName == null) {
             return false;
             
         }
-        if (userName.length() <8||>11) {
+        if (userName.length() <8) {
             return false;
         }
-        if (userName.charAt[0]) {
-            
+        if (userName.length() >11) {
+            return false;
         }
+
+        for (int i = 0; i < userName.length(); i++) {
+            char c = userName.charAt(i);
+            if(i==0 && !isAlpha(c)){
+                return false;
+            }
+            if(!isAlpahNumber(c)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private static boolean isAlpha(char c){
+
+    }
+
+    private static boolean isAlpahNumber(char c){
 
     }
 }
