4b89b9f222fb6c43c99f4ab9a5e80f7bd9208c21
余代起 <2944183739@qq.com>
2021-07-14 11:39:20 +0800

    学号

---
+++ src/main/java/cn/maidaotech/java07/Hhw.java
@@ -0,0 +1,16 @@
+package cn.maidaotech.java07;
+
+public class Hhw {
+    public static void main(String[] args){
+        System.out.println("maidao daojishi");
+        Hhw p = new Hhw();
+        p.Hhw();
+    }
+    
+
+    private void Hhw() {
+        System.out.println("meth");
+    }
+
+    
+}
