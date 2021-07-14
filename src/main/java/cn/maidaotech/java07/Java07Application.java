3848688860c034cb41656ed9989d8b10c18848ae
“zhanghonglei” <2678598229@qq.com>
2021-07-14 16:42:14 +0800

    提交

--- src/main/java/cn/maidaotech/java07/Java07Application.java
+++ src/main/java/cn/maidaotech/java07/Java07Application.java
@@ -1,13 +1,31 @@
 package cn.maidaotech.java07;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.joran.util.StringToObjectConverter;
+
 @SpringBootApplication
 public class Java07Application {
+  
+    public static void zhl(String str) { 
+        String newString=""; 
+        for (int i = 0; i < str.length(); i++) { 
+            newString=newString+str.charAt(str.length()-1-i); 
+        } 
+        System.out.println(newString); 
+    } 
 
-	public static void main(String[] args) {
-		SpringApplication.run(Java07Application.class, args);
-	}
+public static void main(String args[]) {
+   zhl("abcd");
+    
 
-}
+  }
+    
+}   
+	
+
+	
+
