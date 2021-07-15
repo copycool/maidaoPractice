d2f3e95fa166a2f0591040bb3b090a0ca6790841
tianchen <2813360784@qq.com>
2021-07-15 08:54:35 +0800

    Maxmin.java

--- src/main/java/cn/maidaotech/java07/ch02/Maxmin.java
+++ src/main/java/cn/maidaotech/java07/ch02/Maxmin.java
@@ -1,10 +1,32 @@
 package cn.maidaotech.java07.ch02;
-
+import java.util.Scanner;
 public class Maxmin {
     public static void main(String[] args) {
-    int i,j;
-    for(i=1;i<=9;i++){
-        for(j=1;j<=9;j++){
+        Scanner in=new Scanner(System.in);
+        System.out.println("请输入一个数字：");
+        int a=in.nextInt();
+
+        System.out.println("请输入一个数字：");
+        int b=in.nextInt();
+
+        System.out.println("请输入一个数字：");
+        int c=in.nextInt();
+        int max,min;
+        if(a>b){
+            max=a;min=b;
+        }else{
+            max=b;min=a;
+        }
+        if(max<c){
+            max=c;
+        if(min>c){
+            min=c;
+        }
+    }System.out.println("max"+"="+max);
+    System.out.println("min"+"="+min);
+
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=9;j++){
             System.out.print(i+"*"+j+"="+i*j+" ");
         }
         System.out.println( );
