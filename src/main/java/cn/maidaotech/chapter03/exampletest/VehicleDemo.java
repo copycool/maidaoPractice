201c3571e63efdd8b5de322bd6f7f7d3c2552366
李鼎凯 <1803727633@qq.com>
2021-07-16 20:10:30 +0800

    稍作修改的小汽车

--- src/main/java/cn/maidaotech/chapter03/exampletest/VehicleDemo.java
+++ src/main/java/cn/maidaotech/chapter03/exampletest/VehicleDemo.java
@@ -1,7 +1,13 @@
 package cn.maidaotech.chapter03.exampletest;
-
+import cn.maidaotech.chapter03.example.Bus;
+import cn.maidaotech.chapter03.example.Car;
 public class VehicleDemo {
     public static void main(String[] args) {
-        
+        Bus bus = new Bus();
+        Car car = new Car();
+        bus.start();
+        bus.stop();
+        car.start();
+        car.stop();
     }
 }
