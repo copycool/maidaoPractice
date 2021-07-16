60708ae3c9e5edcd2e3bd1b580c8230c3e87660c
guoteng <gtbetter666@163.com>
2021-07-16 17:21:39 +0800

    DogTest

--- src/main/java/cn/Test/DogTest.java
+++ src/main/java/cn/Test/DogTest.java
@@ -5,7 +5,7 @@ import cn.maidaotech.java07.chapter03.Dog;
 public class DogTest {
     public static void main(String[] args) {
         Dog d1 = new Dog("柯基", "黄色", 1);
-        Dog d2 = new Dog("哈士奇", "黑白色", 3);
+        Dog d2 = new Dog("哈士奇", "黑白色", 3); 
         Dog d3 = new Dog("金毛", "蓝色", 2);
         Dog[] dogs = {d1,d2,d3};
         // Dog[] dogs = {new Dog("柯基", "黄色", 1),new Dog("哈士奇", "黑白色", 3),new Dog("金毛", "蓝色", 2)};
