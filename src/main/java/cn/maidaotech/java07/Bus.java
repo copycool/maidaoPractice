a1df972314d97eef9b2d7c6abf7f0d1f56a9e0e5
“zhanghonglei” <2678598229@qq.com>
2021-07-16 21:23:33 +0800

    交通工具

---
+++ src/main/java/cn/maidaotech/java07/Bus.java
@@ -0,0 +1,10 @@
+package cn.maidaotech.java07;
+
+public class Bus implements Vehicle{
+    public void start(){
+        System.out.println("公交车已经启动");
+    } 
+    public void stop(){
+        System.out.println("公交已经停止");
+    }
+}
---
+++ src/main/java/cn/maidaotech/java07/Vehicle.java
@@ -0,0 +1,8 @@
+package cn.maidaotech.java07;
+
+public  interface Vehicle{
+    public void start();
+
+    public void stop();
+
+}
