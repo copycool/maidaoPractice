a45180a0c7e1f9d253048257b7bd8049b94f2aeb
wubing <761506352@qq.com>
2021-07-17 19:34:15 +0800

    更改BUG

---
+++ src/main/java/cn/maidaotech/java07/chapter02/StringUtils.java
@@ -0,0 +1,13 @@
+package cn.maidaotech.java07.chapter02;
+
+public class StringUtils {
+
+    public static boolean isUpperCase(char c) {
+        return false;
+    }
+
+    public static boolean isAlphaNumber(char c) {
+        return false;
+    }
+
+}
