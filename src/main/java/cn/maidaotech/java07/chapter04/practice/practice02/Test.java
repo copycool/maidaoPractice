56f4466bab8e043082c6877e25d2786c12926047
Ghdzj521 <3219483712@qq.com>
2021-07-19 19:22:59 +0800

    chapter 04 task submit

--- src/main/java/cn/maidaotech/java07/chapter04/practice/practice02/Test.java
+++ src/main/java/cn/maidaotech/java07/chapter04/practice/practice02/Test.java
@@ -1,6 +1,7 @@
 package cn.maidaotech.java07.chapter04.practice.practice02;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,20 +25,6 @@ public class Test {
         System.out.println("LinkedList添加元素耗时：" + (end - start));
 
         start = System.currentTimeMillis();
-        for (int i = 99999; i >= 0; i--) {
-            list.remove(i);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("ArrayList删除元素耗时：" + (end - start));
-
-        start = System.currentTimeMillis();
-        for (int i = 999999; i >= 0; i--) {
-            linkedList.remove(i);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("LinkedList删除元素耗时：" + (end - start));
-
-        start = System.currentTimeMillis();
         list.get(list.size() - 1);
         end = System.currentTimeMillis();
         System.out.println("ArrayList从集合尾部访问元素的时间：" + (end - start));
@@ -46,5 +33,24 @@ public class Test {
         linkedList.get(linkedList.size() - 1);
         end = System.currentTimeMillis();
         System.out.println("LinkedList从集合尾部访问元素的时间：" + (end - start));
+
+        start = System.currentTimeMillis();
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("ArrayList删除元素耗时：" + (end - start));
+
+        start = System.currentTimeMillis();
+        Iterator<Integer> iterator2 = linkedList.iterator();
+        while (iterator2.hasNext()) {
+            iterator2.next();
+            iterator2.remove();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("LinkedList删除元素耗时：" + (end - start));
+
     }
 }
