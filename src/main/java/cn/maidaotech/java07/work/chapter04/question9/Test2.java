45aacc83f604e9b4a7240f1043c47a62f5794d99
dawnlizhenwei <1828155655@qq.com>
2021-07-20 09:36:08 +0800

    完善代码

--- src/main/java/cn/maidaotech/java07/work/chapter04/question9/Test2.java
+++ src/main/java/cn/maidaotech/java07/work/chapter04/question9/Test2.java
@@ -17,5 +17,28 @@ public class Test2 {
         for (int i = 0; i < 100; i++) {
             System.out.println(new Random().nextInt(3)+1 );
         }
+        
+    /**
+     * 求最大公约数
+     * @param legth
+     * @param width
+     * @return
+     */
+    // public static Integer maxSideLength(int legth,int width){
+    //     //求出两个数字之间的小值
+    //     int min = legth < width ? legth : width;
+    //     //这种方法运用了数学公式中的辗转相除法
+    //     for (int i = min;i >= 1;i --) {
+    //         if (legth % i == 0 && width % i == 0) {
+    //             return i;
+    //         }
+    //     }
+    //     return 0;
+    // }
+//     int w = 68;
+//     int l = 168;
+//    //假设最大边长为  SideLength 米
+//     Integer sideLength = 0;
+    
     }
 }
