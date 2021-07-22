d56a9072f3772954ef1f3c19b70e105291796b81
zhouyuhang <1350943817@qq.com>
2021-07-22 18:23:40 +0800

    兔子繁殖

---
+++ src/main/java/cn/maidaotech/chapter04/practice11/Rabbit.java
@@ -0,0 +1,24 @@
+package cn.maidaotech.chapter04.practice11;
+/*
+
+一般而言，兔子在出生两个月后，就有繁殖能力，
+一对兔子每个月能生出一对小兔子来。
+如果所有兔子都不死，那么一年后可以繁殖多少对兔子？
+
+*/
+public class Rabbit {
+    public static void main(String[] args) {
+        int i = 1;
+        for (i = 1;i <= 20; i++) {
+            System.out.println("兔子第"+i+"个月的总数为："+num(i));
+        }
+        
+    }
+    public static int num(int x){
+        if(x == 1 || x == 2){
+            return 1;
+        }else{
+            return num(x-1)+num(x-2);
+        }
+    }
+}
