3970676ca1ad66584994d859def9af28b3febe9c
luxiang <1264257976@qq.com>
2021-07-22 17:36:55 +0800

    修改作业

--- demo/src/main/java/com/example/four/exercise6/Integer.java
+++ demo/src/main/java/com/example/four/exercise6/Integer.java
@@ -1,18 +1,22 @@
 package com.example.four.exercise6;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
-
-
+import java.util.List;
+import java.util.Set;
 public class Integer {
-    String s1[];
-  
-   public static void main(String[] args) {
-       
-    String s1 = new String("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20");
+ 
     
-       
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add(s1);
-    System.err.println(hashSet);
+   public static void main(String[] args) {
+    Set<Integer> set = new HashSet<>();
+    while (set.size()<20) {
+        Integer num = (int) (Math.random() * 30 + 1);
+        set.add(num);
+    } 
+        for(Integer i : set){
+            System.out.println(i + "、");
+        }
+    System.err.println("共计："+set.size()+"个");
 
    }
 }
--- demo/src/main/java/com/example/four/exercise7/Sort.java
+++ demo/src/main/java/com/example/four/exercise7/Sort.java
@@ -1,14 +1,18 @@
 package com.example.four.exercise7;
 
-import java.util.ArrayList;
-import java.util.Collection;
+
+import java.util.Arrays;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Sort {
     public static void main(String[] args) {
 		String english = "The identifier of the encoding used to encode the bytes in value";
-		String[] a = english.split(" ");
-		List<String> e = new ArrayList<>();
+		String[] str = english.split(" ");
+		List<String> list = Arrays.asList(str);
+		Collections.sort(list);
+		System.out.println(list);
 		
 	}
 
