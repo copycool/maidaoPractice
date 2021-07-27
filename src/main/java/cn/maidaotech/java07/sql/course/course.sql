04c9542edbf338f3002c969c659a6a3e8b0b9cfd
xxy <1127203451@qq.com>
2021-07-27 17:36:13 +0800

    ch5

--- src/main/java/cn/maidaotech/java07/sql/course/course.sql
+++ src/main/java/cn/maidaotech/java07/sql/course/course.sql
@@ -63,38 +63,47 @@ VALUES
 ('21101','张三',02,98),
 ('21101','张三',03,89),
 ('21101','张三',04,83),
+
 ('21102','李四',01,89),
 ('21102','李四',02,100),
 ('21102','李四',03,86),
 ('21102','李四',04,93),
+
 ('21103','王五',01,65),
 ('21103','王五',02,74),
 ('21103','王五',03,90),
 ('21103','王五',04,76),
+
 ('21104','赵六',01,88),
 ('21104','赵六',02,90),
 ('21104','赵六',03,93),
 ('21104','赵六',04,84),
+
 ('21105','苏七',01,90),
 ('21105','苏七',02,80),
 ('21105','苏七',03,97),
 ('21105','苏七',04,81),
+
 ('21201','李冰冰',01,81),
 ('21201','李冰冰',02,64),
 ('21201','李冰冰',03,89),
 ('21201','李冰冰',04,70),
+
 ('21202','张丽丽',01,93),
 ('21202','张丽丽',02,90),
 ('21202','张丽丽',03,89),
 ('21202','张丽丽',04,89),
+
 ('21203','刘西西',01,65),
 ('21203','刘西西',02,76),
 ('21203','刘西西',03,98),
 ('21203','刘西西',04,79),
+
 ('21204','白落落',01,85),
 ('21204','白落落',02,83),
 ('21204','白落落',03,95),
 ('21204','白落落',04,90),
+
 ('21205','沈蔓蔓',01,92),
 ('21205','沈蔓蔓',02,75),
 ('21205','沈蔓蔓',03,84),
@@ -107,16 +116,23 @@ update student set name='张大大' where sno='21101';
 update score set chinese_score= 69 where sno='21101';
 /* a. 统计每门课的学生人数；  */
 SELECT cno '课程编号' ,COUNT(sno) '选课人数' FROM score group BY cno;
-
 /* b. 查询某一门课程不及格的学生信息；  */
-
-/* c. 查询某一个学生的各科成绩；  */
+SELECT * from student where sno in(SELECT sno from score where cno=01 and score<60);
+/* c. 查询某一个学生的各科成绩*/
+select * from score where sno in(select sno from student where name="张三");
 /* d. 查询一个班级每个学生的总成绩，并按总成绩由高到低排序；  */
+SELECT name,SUM(score) from score where name IN
+(select name from student where class="一班") 
+GROUP by name 
+ORDER BY SUM(score) desc;
 /* e. 查询每门课都大于 80 分的学生信息；  */
+SELECT * from student where sno in (select sno FROM score  as s GROUP BY s.sno Having MIN(s.score)>80 );
 /* f. 查询至少有一门课程不及格的学生信息；  */
+SELECT * from student where sno in (select sno FROM score  as s GROUP BY s.sno Having MIN(s.score)<60 );
 /* g. 查询某一门课各个班级的平均学生成绩，并按成绩由高到低排序；  */
+select cno,avg(score) from score group BY cno order BY avg(score) desc;
 /* h. 查询各科成绩最高分、最低分和平均分；  */
-
+select cno,avg(score),min(score),max(score) from score group BY cno ;
 
 
 
---
+++ src/main/java/cn/maidaotech/java07/sql/data.sql
@@ -0,0 +1,106 @@
+DROP TABLE IF EXISTS `big_data`;
+CREATE TABLE `big_data`(
+    `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "自增主键",
+  `user_id` BIGINT(20) UNSIGNED NOT NULL COMMENT "用户主键",
+    `username` VARCHAR(50) NOT NULL COMMENT "用户名",
+    `amount` INT(11) NOT NULL COMMENT "年龄",
+    `status` TINYINT(1) UNSIGNED NOT NULL COMMENT "状态",
+    `created_at` BIGINT(20) NOT NULL COMMENT "创建时间戳",
+    `update_at` BIGINT(20) NULL COMMENT "修改时间戳",
+  PRIMARY KEY(`id`) USING BTREE
+)ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT="大数据";
+ALTER TABLE `big_data` CHANGE `username` `username` VARCHAR(50) CHARACTER SET utf8mb4;
+
+/* 一个函数插入200万条数据 */
+set global log_bin_trust_function_creators=1;
+DELIMITER $$
+CREATE PROCEDURE insert_data(IN START INT(10),IN max_num INT(10))  
+BEGIN  
+DECLARE i INT DEFAULT 0;   
+#set autocommit =0 把autocommit设置成0  
+ SET autocommit = 0;    
+ REPEAT  
+ SET i = i + 1;  
+ INSERT INTO big_data (user_id,username,amount,status,created_at,update_at  ) 
+VALUES (CONCAT('1',CEILING(RAND()*90000+10000)),uuid(),FLOOR(18 + (RAND() * 7)),floor(rand()*2),CONCAT('1',CEILING(RAND()*900000000+100000000)),CONCAT('1',CEILING(RAND()*900000000+100000000)));  
+ UNTIL i = max_num  
+ END REPEAT;  
+ COMMIT;  
+ END $$
+
+DELIMITER ;
+CALL insert_data(10001,2000000);
+
+/* I */
+EXPLAIN SELECT * 
+FROM `big_data` 
+ORDER BY `created_at`
+DESC
+
+/* II */
+EXPLAIN SELECT * 
+FROM `big_data`
+ORDER BY `username`
+ASC
+
+/* III */
+EXPLAIN SELECT *
+FROM `big_data`
+WHERE
+id=116136
+ORDER BY created_at
+DESC
+
+/* IV */
+EXPLAIN SELECT *
+FROM `big_data`
+WHERE
+user_id=116136
+AND
+`status` IN (1,2)
+ORDER BY created_at
+DESC
+
+/* V */
+SELECT *
+FROM `big_data`
+WHERE
+id=116136
+AND
+amount > 5000
+ORDER BY created_at
+DESC
+
+/* VI */
+SELECT *
+FROM `big_data`
+WHERE
+id=116136
+AND
+`status` = 1
+AND
+created_at >= unix_timestamp('2018-1-1 01-01-01')
+ORDER BY created_at
+DESC
+
+/* VII */
+SELECT *
+FROM `big_data`
+WHERE
+username="mbk"
+AND
+`status`=5
+ORDER BY created_at
+DESC
+
+/* VIII */
+SELECT COUNT(*) AS "总数",SUM(amount) AS "总金额",`status`
+FROM `big_da
+FROM `big_data`
+GROUP BY `status`
+
+/* 索引 */
+ALTER TABLE `big_data` ADD KEY `idx_userid`(user_id);
+ALTER TABLE `big_data` ADD KEY `idx_created`(created_at);
+ALTER TABLE `big_data` ADD KEY `idx_username`(username);
+ALTER TABLE `big_data` ADD KEY `idx_amount`(amount);
