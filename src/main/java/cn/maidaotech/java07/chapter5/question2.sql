d0707116a8c6de8eb1d761467f5450407ab0cf68
xumengmeng <776868594@qq.com>
2021-07-27 16:05:27 +0800

    ch5

--- src/main/java/cn/maidaotech/java07/chapter5/question2.sql
+++ src/main/java/cn/maidaotech/java07/chapter5/question2.sql
@@ -1,43 +1,64 @@
-/*
-创建一张学生表student
-其中有字段id、first_name、last_name、sex、school等列
-其中id为自增主键，通过SQL完成以下操作：
-如何从表中选取first_name列的值以"a"开头的所有记录？
-如何从表中选择first_name等于Tom而且last_name等于Peter的所有记录？
-如何按字母顺序从表中选取first_name介于Angela和Peter的所有记录？
-哪条SQL语句可返回唯一不同的值？
-SELECT DIFFERENT
-SELECT DISTINCT
-SELECT UNIQUE
-如何根据first_name列降序地从表中返回所有记录？只返回前十条记录呢？第11到20条记录呢？
-如何向表中插入新的记录？
-如何向表中的last_name列插入Wilson？
-如何把表中last_name列的Peter改为Wilson？
-如何在表中删除first_name等于Angela的记录？
-如何返回表中记录的数目？
-如何在已存在的student表中增加一列名为age表示年龄的列？
-如何修改first_name列的字符长度为50且不允许NULL值？如何删除表中的age列？
-如何从将表中所有数据的first_name和last_name以空格连接合并存入另外一张表persion中*/
-CREATE TABLE IF NOT EXISTS `student` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    'first_name' varchar(30) comment '姓',
-    'last_name' varchar(30) comment '名',
-    sex enum('男','女') not null,
-    'school' varchar(30) comment '学校',
-    PRIMARY KEY (`id`),
-    )ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
+ CREATE TABLE
+IF
+	NOT EXISTS `student` (
+		`id` BIGINT ( 20 ) NOT NULL auto_increment,
+		`first_name` VARCHAR ( 30 ) CHARACTER 
+		SET utf8 DEFAULT NULL COMMENT '名',
+		`last_name` VARCHAR ( 30 ) CHARACTER 
+		SET utf8 DEFAULT NULL COMMENT '姓',
+		`sex` TINYINT ( 2 ) COMMENT '性别',
+		`school` VARCHAR ( 30 ) COMMENT '学校',
+		PRIMARY KEY ( `id` ) 
+	) ENGINE = INNODB DEFAULT charset = utf8;
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'Tom', 'Peter', '0','12345' );
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'aom', 'Peter', '1','12345' );
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'Angela', 'Peter', '0','12345' );
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'Peter', 'Peter', '1','12345' );
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'Tom', 'Hany', '1','12345' );
+INSERT INTO student ( first_name, last_name, sex, school )
+VALUES
+	( 'Sastu', 'Peter', '1','12345' );
+SELECT
+	* 
+FROM
+	student 
+WHERE
+	`first_name` LIKE 'a%';
+SELECT
+	* 
+FROM
+	student 
+WHERE
+	`first_name` = 'Tom' 
+	AND `last_name` = 'Peter';
+SELECT *FROM student where `first_name`>'Angela'and`first_name`<'Peter' ORDER BY 'first_name';
+select DISTINCT `last_name` from student;
+SELECT*from student ORDER BY `first_name` desc;
+select *from student ORDER BY`first_name` desc limit 0,10;
+select *from student ORDER BY`first_name` desc limit 10,10;
+insert into student(first_name,last_name,sex,school)
+values('aTome','Peter',1,'12345');
 
-    insert into student(first_name,last_name,sex,school)
-    values('Tom','Peter','女'，'北');
-    insert into student(first_name,last_name,sex,school)
-    values('almom','Peter','女'，'北');
-    insert into student(first_name,last_name,sex,school)
-    values('Angela','Peter','女'，'北');
-    insert into student(first_name,last_name,sex,school)
-    values('Peter','Pjsjk','女'，'北');
+insert into student(last_name) values ('Wilson');
 
-    SELECT *from student where first_name like "a%";
-    
+update student set last_name='Wilson' where last_name='Peter';
 
+delete from student where first_name='angela';
 
+select count(*) count from student;
 
+alter table student add `age` tinyint(1) unsigned comment '年龄';
+
+alter table student modify first_name varchar(50);
+
+alter table student drop age;
---
+++ src/main/java/cn/maidaotech/java07/chapter5/question7.sql
@@ -0,0 +1,110 @@
+CREATE TABLE sstudent (
+	`id` BIGINT ( 20 ) NOT NULL auto_increment,
+	`sno` VARCHAR ( 30 ) CHARACTER 
+	SET utf8 DEFAULT NULL COMMENT '学号',
+	`sna` VARCHAR ( 30 ) CHARACTER 
+	SET utf8 DEFAULT NULL COMMENT '姓名',
+	`ssex` TINYINT ( 2 ) COMMENT '性别',
+	PRIMARY KEY ( `id` ) 
+)ENGINE = INNODB DEFAULT charset = utf8;
+ INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201801', '江一', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201802', '江二', '0' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201803', '江河', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201804', '江四', '0' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201805', '江五', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201806', '江六', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201807', '江七', '0' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201808', '江八', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201809', '江九', '1' );
+	INSERT INTO sstudent ( sno, sna, ssex )
+VALUES
+	( '201810', '江十', '1' );
+
+create table class(
+`id` BIGINT ( 20 ) NOT NULL auto_increment,
+`cnumber`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '班号',
+`cname`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '班级名称',
+PRIMARY KEY (`id`)
+)ENGINE = INNODB DEFAULT charset = utf8;
+INSERT INTO class ( cnumber, cname)
+VALUES
+	( '01', '软工41' );
+	INSERT INTO class ( cnumber, cname)
+VALUES
+	( '02', '软工42' );
+
+create table course(
+`id` BIGINT ( 20 ) NOT NULL auto_increment,
+`conumber`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '课程号',
+`coname`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '课程名称',
+`cocharacter`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '课程性质',
+PRIMARY KEY (`id`)
+);
+INSERT INTO course ( conumber, coname, cocharacter )
+VALUES
+	( '001', '计算机导论', '选修课' );
+INSERT INTO course ( conumber, coname, cocharacter )
+VALUES
+	( '002', '数据结构', '必修课' );
+INSERT INTO course ( conumber, coname, cocharacter )
+VALUES
+	( '003', '软件工程', '必修课' );
+
+create table score(
+`id` BIGINT ( 20 ) NOT NULL auto_increment,
+`cou_id`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '课程号',
+`stu_id`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '学号',
+`sscore`  VARCHAR ( 30 )  CHARACTER SET utf8 DEFAULT NULL COMMENT '成绩',
+PRIMARY KEY (`id`)
+);
+INSERT INTO score(cou_id,stu_id,sscore)VALUES('001','201801','90');
+INSERT INTO score(cou_id,stu_id,sscore)VALUES('002','201810','40');
+
+CREATE TABLE stu_class(
+`id` BIGINT(20)NOT NULL auto_increment,
+`stu_id` VARCHAR(30) CHARACTER SET utf8 default null comment'学号',
+`cla_id` VARCHAR(30) CHARACTER SET utf8 default  null comment'班级号',
+PRIMARY KEY(`id`)
+)ENGINE=INNODB DEFAULT charset=utf8;
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201801','01');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201802','01');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201803','01');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201804','01');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201805','01');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201806','02');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201807','02');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201808','02');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201809','02');
+INSERT INTO stu_class(stu_id,cla_id)VALUES('201810','02');
+
+
+CREATE TABLE stu_course(
+`id` BIGINT(20) not null auto_increment,
+`stu_id` VARCHAR (30) CHARACTER SET utf8 DEFAULT NULL COMMENT '学号',
+`cou_id` VARCHAR (30) CHARACTER SET utf8 DEFAULT NULL COMMENT '课程号',
+PRIMARY KEY(`id`)
+);
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201801','001');
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201801','002');
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201801','003');
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201802','001');
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201803','001');
+INSERT INTO stu_course(stu_id,cou_id) VALUES('201804','001');
