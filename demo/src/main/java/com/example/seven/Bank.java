76eae70cb53b8e6d2d8696017fe756b358d9652a
luxiang <1264257976@qq.com>
2021-07-28 22:17:31 +0800

    修改

--- demo/src/main/java/com/example/seven/Bank.java
+++ demo/src/main/java/com/example/seven/Bank.java
@@ -1,22 +1,36 @@
 package com.example.seven;
 
 public class Bank extends Thread{
-    private static int money = 0;
+    private Object object;
+    
     private int a = 20;
+
+    public Bank(Object o){
+        super();
+        this.object = o;
+    }
     @Override
     public void run() {
-        if (money == 0) {
-            System.out.println("存了"+a+"元"); 
-            money=money+a; 
-        }else{
-            System.out.println("取了"+a+"元");
+        try {
+            synchronized(object){
+                System.out.println("存了"+a+"元");
+                object.wait();
+                System.out.println("取了"+a+"元");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     public static void main(String[] args) {
-        Bank A = new Bank();
-        Bank B = new Bank();
-
-        A.start();
-        B.start();
+        try {
+            Object object = new Object();
+            Bank A = new Bank(object);
+            A.start();
+            Thread.sleep(300);
+            Bank B = new Bank(object);
+            B.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
