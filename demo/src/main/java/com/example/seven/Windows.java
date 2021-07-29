1beb0e093a6795f2144aa379ec3e5ff9143d4f48
luxiang <1264257976@qq.com>
2021-07-29 19:47:03 +0800

    修改作业

--- demo/src/main/java/com/example/seven/Windows.java
+++ demo/src/main/java/com/example/seven/Windows.java
@@ -1,28 +1,43 @@
 package com.example.seven;
 
-public class Windows extends Thread{
+public class Windows extends Thread {
     private static int ticket = 100;
+    Object obj = new Object();
+    
+
     @Override
     public void run() {
-        synchronized(this){
-        while (ticket>0) {
-            System.out.println(getName()+"：卖票，剩"+ticket--);              
+
+        while (true) {
+            synchronized (obj) {
+               if (ticket>0) {
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+"：卖票，票号为："+ticket);
+                ticket--;
+               }else{
+                break;
+               }
             }
         }
-    
     }
     public static void main(String[] args) {
         Windows t1 = new Windows();
-        Windows t2 = new Windows();
-        Windows t3 = new Windows();
+        
+        Thread w =  new Thread(t1);
 
-        t1.setName("窗口1");
-        t2.setName("窗口2");
-        t3.setName("窗口3");
 
-        t1.start();
-        t2.start();
-        t3.start();
+        w.setName("窗口1");
+        w.setName("窗口2");
+        w.setName("窗口3");
+
+        w.start();
+        w.start();
+        w.start();
     }
     
 }
